94b5b71b324207570c33f5fd402844f0f6467829
Rob_McBryde <robert.mcbryde@ros.gov.uk>
2020-12-01 15:02:29 +0000

    addition of env ports for running in prod

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ app.use(express.static(__dirname + '/public'));
 
 app.use(bodyParser.urlencoded({extended: false}))
 app.use(bodyParser.json())
-const port = 3000
+const port = process.env.PORT || 3000
 
 const logs = []
 
